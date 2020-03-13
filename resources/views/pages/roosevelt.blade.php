762debd59ada2995743af1a2a9cb1bd50a9ecde1
anthonyMars <anthony.marstiller@gmail.com>
2020-03-13 10:28:14 -0400

    Update roosevelt.blade.php

--- resources/views/pages/roosevelt.blade.php
+++ resources/views/pages/roosevelt.blade.php
@@ -48,7 +48,7 @@
                          <li> Tuesday, Wednesday, Friday 2:30-6:00pm: - Open Gym</li>
                          <li> Tuesday and Friday 6:00-8:00pm: 1/2 court open gym for everyone & 1/2 court pickleball  </li>
                            <h3>Special Events</h3>
-                         <li>March 29 - GlowBall (Glow in the dark basketball) 6:00-8:00pm  / Free Pick up games for Middle & High School students </li>
+                         <li>March 29 - GlowBall 6:00-8:00pm / Free Pick up games for Middle & High School students </li>
                          <li>Wednesday 9:00am-1:00pm: Senior Program </li>
                          <li>Wednesday 6:00-8:00:  WV Roller Derby Practice </li> 
                       
