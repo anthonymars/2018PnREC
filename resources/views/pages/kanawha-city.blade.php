0b5cde182aec126cd15140ccdd69f4669c0a4aec
Info@cityofcharleston.org <erodedava@yahoo.com>
2021-02-24 14:27:50 -0500

    Update kanawha-city.blade.php

--- resources/views/pages/kanawha-city.blade.php
+++ resources/views/pages/kanawha-city.blade.php
@@ -67,9 +67,9 @@
                             <br />(304)348-6484
                             <br /><strong>Hours:</strong>
                             <!--<br />Monday - Friday 9am - 9pm -->
-                            <br /> NOTE:Due to COVID-19, Kanawha City Community Center is closed indefinitely!
+                            <br /><strong> NOTE:Due to COVID-19, Kanawha City Community Center is closed indefinitely!</strong>
                             <!-- <br />Saturday 9am - 5pm -->
-                            <br />Sunday Closed
+                           <!-- <br />Sunday Closed -->
                             <br /><strong>Pool Hours:</strong>
                             <br />Tuesday - Sunday 12pm - 6pm
                             <br />Lap Swimming 9am - 11am
