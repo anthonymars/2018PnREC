7add824e436be7f78ffb51eea0eb4f809d4e4b1d
Info@cityofcharleston.org <erodedava@yahoo.com>
2021-02-25 11:32:26 -0500

    Update mkl-jr.blade.php
    NOTE: Due to COVID-19, Kanawha City Community Center is closed indefinitely!

--- resources/views/pages/mkl-jr.blade.php
+++ resources/views/pages/mkl-jr.blade.php
@@ -71,11 +71,12 @@
                             <br />Charleston, WV 25301
                             <br />(304)348-6404
                             <br /><strong>Hours:</strong>
-                            <br />Monday - Friday 9am - 9pm
+                          <br />  NOTE: Due to COVID-19, Kanawha City Community Center is closed indefinitely! 
+                            <!-- <br />Monday - Friday 9am - 9pm
                             <br />Saturday 9am - 5pm
                             <br />Sunday Closed
                             <br /><strong>Pool Hours:</strong>
-                            <br />Tuesday - Sunday 12pm - 6pm
+                            <br />Tuesday - Sunday 12pm - 6pm -->
                             <br /><strong>Recreation Center Manager:</strong>
                             <br /><a href="mailto:jennifer.holley@cityofcharleston.org"><i class="fa fa-envelope"></i> Dax Miller </a></small>
                             <br /><strong>Program Coordinator:</strong>
