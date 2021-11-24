45214697cad4afa6800221c535288a6b9f241342
Info@cityofcharleston.org <erodedava@yahoo.com>
2021-11-24 09:29:23 -0500

    Update kanawha-city.blade.php

--- resources/views/pages/kanawha-city.blade.php
+++ resources/views/pages/kanawha-city.blade.php
@@ -46,7 +46,7 @@
                         </p>
                 
                     </div>
-                   
+                    </div>
                     
              
                 <div class="col-md-4">
