7b81861daecf9f95c1921ca4fb262fdc7f5220fb
Info@cityofcharleston.org <erodedava@yahoo.com>
2022-10-25 11:40:55 -0400

    Update weekly-fun.blade.php

--- resources/views/pages/weekly-fun.blade.php
+++ resources/views/pages/weekly-fun.blade.php
@@ -197,8 +197,8 @@
             <h2 class="text-center">
                 Share Your Experience
             </h2>
-            <p class="text-center"><span class="thick500">Follow us</span> on Facebook, Instagram, Pinterest and Twitter! Use the hashtag <strong>#getactivecwv</strong> to share your experiences.</p>
-            <p class="text-center"><a href="https://www.facebook.com/cwvparksandrecreation"><i class="fa fa-facebook-square fa-2x"></i></a> <a href="https://www.instagram.com/cwvparksandrecreation/"><i class="fa fa-instagram fa-2x"></i></a> <a href="https://www.pinterest.com/cwvparks/"><i class="fa fa-pinterest fa-2x"></i></a> <a href="https://twitter.com/cwvparks"><i class="fa fa-twitter fa-2x"></i></a></p>
+            <p class="text-center"><span class="thick500"></span>  Use strong>#getactivecwv</strong> to share your experiences on Facebook, Instagram and Twitter!</p>
+           <!-- <p class="text-center"><a href="https://www.facebook.com/cwvparksandrecreation"><i class="fa fa-facebook-square fa-2x"></i></a> <a href="https://www.instagram.com/cwvparksandrecreation/"><i class="fa fa-instagram fa-2x"></i></a> <a href="https://www.pinterest.com/cwvparks/"><i class="fa fa-pinterest fa-2x"></i></a> <a href="https://twitter.com/cwvparks"><i class="fa fa-twitter fa-2x"></i></a></p>-->
         </div>
 
     </section>
