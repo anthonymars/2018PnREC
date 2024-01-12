36e83879b1efe2b34b6e9684809bbbbe0390ffca
Info@cityofcharleston.org <erodedava@yahoo.com>
2024-01-12 14:21:06 -0500

    Update kanawha-city.blade.php

--- resources/views/pages/kanawha-city.blade.php
+++ resources/views/pages/kanawha-city.blade.php
@@ -61,7 +61,7 @@
                             <br /><strong>Program Coordinator: </strong>
                              <br /><a href="mailto:T'Andre.Boxley@cityofcharleston.org"><i class="fa fa-envelope"></i>  T’Andre Boxley</a>
                              <br /><a href="mailto:Kelly.Jones@cityofcharleston.org"><i class="fa fa-envelope"></i> Kelly Jones</a>
-                         <br /><strong> AssistantProgram Coordinator: </strong>
+                         <br /><strong> Assistant Program Coordinator: </strong>
                             <br />  <a href="mailto:Benjamin.grady@cityofcharleston.org"> <i class="fa fa-envelope"></i>Ben Grady</a>
              </small></p>
 
