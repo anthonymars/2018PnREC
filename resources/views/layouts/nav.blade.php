91f657d84e912447538832c1d0880ac06063eb23
Info@cityofcharleston.org <erodedava@yahoo.com>
2024-01-12 14:11:56 -0500

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -24,7 +24,7 @@
                         <a class="dropdown-item" href="/mlk-jr"><strong>Martin Luther King Jr.</strong></a>
                         <a class="dropdown-item" href="/north-charleston"><strong>North Charleston</strong></a>
                         <a class="dropdown-item" href="/roosevelt"><strong>Roosevelt</strong></a>
-                        <div class="dropdown-divider"></div>
+                       <!-- <div class="dropdown-divider"></div> -->
                       <!--  <a class="dropdown-item" href="/galleries"><strong>Gallery</strong></a> -->
                     </div>
                 </li>
