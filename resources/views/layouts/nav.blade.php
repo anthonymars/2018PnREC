4c1f3b40ec824b16ca9a131806336f49f4292e79
Info@cityofcharleston.org <erodedava@yahoo.com>
2024-01-12 12:18:07 -0500

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -25,7 +25,7 @@
                         <a class="dropdown-item" href="/north-charleston"><strong>North Charleston</strong></a>
                         <a class="dropdown-item" href="/roosevelt"><strong>Roosevelt</strong></a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="/galleries"><strong>Gallery</strong></a>
+                      <!--  <a class="dropdown-item" href="/galleries"><strong>Gallery</strong></a> -->
                     </div>
                 </li>
                 <li class="nav-item dropdown">
