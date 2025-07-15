af177a3b7d406510608734b470a07a126a0aa5a6
Info@cityofcharleston.org <erodedava@yahoo.com>
2025-07-15 15:06:58 -0400

    Update coming-soon.blade.php

--- resources/views/pages/coming-soon.blade.php
+++ resources/views/pages/coming-soon.blade.php
@@ -9,7 +9,7 @@
     <section id="info" class="py-5">
         <div class="container py-5">
            <h2 class="text-center">Local Events</h2>  
-            <h2 class="text-center"><a href="https://events.charlestonwv.com/event/create" style="color: red;">To submit an event, please visit</a></div></section></h2>
+            <h2 class="text-center"><a href="https://events.charlestonwv.com/event/create" style="color: red;">To submit an event, please visit</a></h2>
               <h2 class="text-center">
             <img src="{{ asset('/images/logos/parksnRec.png') }}" alt="" class="img-fluid mx-auto d-block">
                     <h4 class="text-left"> 
